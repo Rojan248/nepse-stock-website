edbf09954ac78993e7b68fb8db701656610fe4f5
Rojan <Rojanrkafle@gmail.com>
2026-01-09 21:42:31 +0545

    chore: Add market summary migration with index fields

---
+++ backend/prisma/migrations/20260109024021_update_market_summary/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "MarketSummary" ADD COLUMN "indexChange" REAL;
+ALTER TABLE "MarketSummary" ADD COLUMN "indexChangePercent" REAL;
+ALTER TABLE "MarketSummary" ADD COLUMN "indexValue" REAL;
